616bd42cbe29bed2a63a120f957111067a09ab85
Hikaru Ikegami <hikaru@HikarunoMacBook-Pro.local>
2017-06-08 22:07:04 +0900

    初期コミット

---
+++ spec/my-test01-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyTest01View from '../lib/my-test01-view';
+
+describe('MyTest01View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
